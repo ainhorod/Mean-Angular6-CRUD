4791a9badf6a74031481be002a5fd175df5c3b56
Ainhoa RMA <34682564+ainhorod@users.noreply.github.com>
2018-05-24 16:38:05 +0200

    Add files via upload

---
+++ src/app/book-detail/book-detail.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookDetailComponent } from './book-detail.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+describe('BookDetailComponent', () => {
+  let component: BookDetailComponent;
+  let fixture: ComponentFixture<BookDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookDetailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
